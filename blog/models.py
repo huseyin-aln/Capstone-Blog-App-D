04ef31bf156910bc8bf887b9397fac9db1d5aba2
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-10-10 11:18:02 +0300

    added pre_save

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+def user_directory_path(instance, filename):
+    return 'blog/{0}/{1}'.format(instance.author.id, filename)
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
@@ -20,7 +23,7 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to=user_directory_path, default='avatar.png')
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
