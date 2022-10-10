04d707dd12219e2ee8cc4d8499f0f9f7f7cf4352
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-10-10 10:48:36 +0300

    added media

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,13 @@
+from tabnanny import verbose
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
@@ -21,5 +25,8 @@ class Post(models.Model):
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    status = models.CharField(max_length=10, choices=OPTIONS)
+    status = models.CharField(max_length=10, choices=OPTIONS, default='D')
     slug = models.SlugField(blank=True, unique=True)
+
+    def __str__(self):
+        return self.title
